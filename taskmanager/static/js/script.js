7499bf835d4d857d37c253758456f52ca241076d
flashdrag <flashdrag@gmail.com>
2023-04-11 21:03:25 +0100

    Add footer and main container to base template

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,7 @@
 //  initialize the sidenav-trigger line into the navbar
-$(document).ready(function(){
+$(document).ready(function () {
     $('.sidenav').sidenav();
-  });
+});
+
+// update copyright year in the footer
+$("#copyright").text(new Date().getFullYear());
